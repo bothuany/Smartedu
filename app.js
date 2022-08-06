cfb4f3b11ae465c5afeffbd25aaa38eaa9916362
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2022-08-07 00:53:06 +0300

    Update

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
+
+//Connect DB
+mongoose
+  .connect("mongodb://localhost/smartedu-db")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
+//Global Variables
+global.userIN = null;
+
 //MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/smartedu-db" }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride("_method", {
+    methods: ["GET", "POST"],
+  })
+);
 
 //ROUTES
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   res.status(200).render("index", {
     page_name: "index",
@@ -8,18 +10,67 @@ exports.getAboutPage = (req, res) => {
     page_name: "about",
   });
 };
+exports.getRegisterPage = (req, res) => {
+  res.status(200).render("register", {
+    page_name: "register",
+  });
+};
+exports.getLoginPage = (req, res) => {
+  res.status(200).render("login", {
+    page_name: "login",
+  });
+};
+
 exports.getContactPage = (req, res) => {
   res.status(200).render("contact", {
     page_name: "contact",
   });
 };
-exports.getCoursesPage = (req, res) => {
-  res.status(200).render("courses", {
-    page_name: "courses",
-  });
-};
-exports.getDashboardPage = (req, res) => {
-  res.status(200).render("dashboard", {
-    page_name: "dashboard",
-  });
+exports.sendEmail = async (req, res) => {
+  try {
+    const outputMessage = `
+  <h1>Mail Details</h1>
+
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+  `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "rbdikmen@gmail.com", // gmail account
+        pass: "eazvohuitaemikev", // gmail password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: 'Smart Edu Contact Form" <rbdikmen@gmail.com>', // sender address
+      to: "viperoynarim@gmail.com", // list of receivers
+      subject: "Smart Edu Contact Form New Message", // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    req.flash("success", "We received your message successfully");
+
+    res.status(200).redirect("/contact");
+  } catch (error) {
+    //req.flash("error", `Something went wrong! ${error.message}`);
+    req.flash("error", `Something went wrong!`);
+    res.status(200).redirect("/contact");
+  }
 };
