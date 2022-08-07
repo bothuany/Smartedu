5ad6a2f932f70334de6caaff3805ef0c0fc2c707
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2022-08-07 15:14:43 +0300

    Final

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,14 +3,30 @@ const Category = require("../models/Category");
 exports.createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: "success",
-      category,
-    });
+    res.status(200).redirect("/users/dashboard");
   } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
+    const errors = validationResult(req);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash("error", errors.array()[i].msg);
+    }
+
+    res.status(400).redirect("/users/dashboard");
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    await Category.findByIdAndRemove(req.params.id);
+
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    const errors = validationResult(req);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash("error", errors.array()[i].msg);
+    }
+
+    res.status(400).redirect("/users/dashboard");
   }
 };
